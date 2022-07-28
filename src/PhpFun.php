6cb169edd225c3ad3767e97637d34f980cc9bc64
songxuan <1668325326@qq.com>
2022-07-28 18:01:55 +0800

    更新

--- src/PhpFun.php
+++ src/PhpFun.php
@@ -15,7 +15,8 @@ namespace SongXuan;
 class PhpFun{
     /**
      * uuid  生成32位以内随机字符串
-     * $len  长度
+     * @param  int $len  长度
+     * @return string
      */
     static function uuid32($len = 10){
         $str = md5(uniqid(mt_rand(), true));
@@ -24,7 +25,8 @@ class PhpFun{
 
     /**
      * 手机号正则
-     * $phone  11 位手机号
+     * @param int $phone  11 位手机号
+     * @return bool
      */
     static function isPhone($phone){
         $myreg='/^1[3-9]\d{9}$/';
@@ -38,7 +40,8 @@ class PhpFun{
 
     /**
      * 邮箱正则
-     * $email  邮箱号
+     * @param  string $email  邮箱号
+     * @return bool
      */
     static function isEmail($email){
         $myreg = '/^[_a-z0-9-]+(\.[_a-z0-9-]+)*@[a-z0-9-]+(\.[a-z0-9-]+)*(\.[a-z]{2,})$/';
@@ -47,6 +50,34 @@ class PhpFun{
         } else {
             return true;
         }
+    }
+
+    /**
+     * 二维数组根据某个字段排序
+     * @param array  $array 要排序的数组
+     * @param string $key 要排序的键
+     * @param string $sort  排序类型 SORT_ASC SORT_DESC
+     * @return array 排序后的数组
+     */
+    static function arraySort($array, $key, $sort = SORT_ASC){
+        $keysValue = array();
+        foreach ($array as $k => $v) {
+            $keysValue[$k] = $v[$key];
+        }
+        array_multisort($keysValue, $sort, $array);
+        return $array;
+    }
+
+    /**
+     * 截取指定两个字符之间字符串
+     * @param  string $input      被截取的字符串
+     * @param  string $start      开始
+     * @param  string $end        结束
+     * @return string
+     */
+    static function strCut($input, $start, $end) {
+        $substr = substr($input, strlen($start)+strpos($input, $start),(strlen($input) - strpos($input, $end))*(-1));
+        return $substr;
     }
 
 
